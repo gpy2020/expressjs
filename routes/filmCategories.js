1154a8de345e95391f69aaee3c0b2aa48d73cf83
Mikhail Anishchanka <vistrible@yandex.ru>
2018-08-15 12:57:36 +0300

    completed task3

--- routes/filmCategories.js
+++ routes/filmCategories.js
@@ -1,21 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require("joi");
 
 router.use(express.urlencoded());
 router.use(express.json());
 
+const categories = [];
+
+const schemaCategory = Joi.object().keys({
+  id: Joi.required(),
+  title: Joi.string()
+    .min(3)
+    .required(),
+  description: Joi.string()
+    .min(3)
+    .max(500)
+    .required(),
+  films: Joi.array()
+    .items(Joi.number())
+    .required()
+});
+
 router
   .route("/categories")
   .get((req, res, next) => {
-    res.send([]);
+    res.send(categories);
   })
   .post((req, res, next) => {
-    res.json(req.body);
+    Joi.validate(req.body, schemaCategory, (err, value) => {
+      if (err) {
+        res.status(400).send(`${res.statusCode}: ${err.message}`);
+      } else {
+        res.json(req.body);
+      }
+    });
   });
 router
   .route("/categories/:id")
   .put((req, res, next) => {
-    res.json(value);
+    Joi.validate(req.body, schemaCategory, (err, value) => {
+      if (err) {
+        res.status(400).send(err.message);
+      } else {
+        res.json(value);
+      }
+    });
   })
   .delete((req, res, next) => {
     res.json({ success: true, id: req.params.id });
--- routes/films.js
+++ routes/films.js
@@ -1,6 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const filmCategories = require("./filmCategories");
+const Joi = require("joi");
+
+const schemaFilm = Joi.object().keys({
+  id: Joi.string().required(),
+  title: Joi.string()
+    .required()
+    .min(3),
+  description: Joi.string()
+    .required()
+    .min(3)
+    .max(500),
+  rating: Joi.number()
+    .min(0)
+    .max(5),
+  category: Joi.string(),
+  avatar: Joi.string()
+    .uri()
+    .required(),
+  galery: Joi.array().items(
+    Joi.string()
+      .uri()
+      .required(),
+    Joi.string()
+      .uri()
+      .required(),
+    Joi.string()
+      .uri()
+      .required(),
+    Joi.string()
+      .uri()
+      .required()
+  )
+});
 
 router.use(express.urlencoded());
 router.use(express.json());
@@ -11,13 +44,25 @@ router
     res.send([]);
   })
   .post((req, res, next) => {
-    res.json(value);
+    Joi.validate(req.body, schemaFilm, (err, value) => {
+      if (err) {
+        res.status(400).send(`${res.statusCode}: ${err.message}`);
+      } else {
+        res.json(value);
+      }
+    });
   });
 
 router
   .route("/films/:id")
   .put((req, res, next) => {
-    res.json(value);
+    Joi.validate(req.body, schemaFilm, (err, value) => {
+      if (err) {
+        res.status(400).send(`${res.statusCode}: ${err.message}`);
+      } else {
+        res.json(value);
+      }
+    });
   })
   .delete((req, res, next) => {
     res.json({ success: true, id: req.params.id });
