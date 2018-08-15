7422e66bea4bf7f536b7e10a3f09d260c1462f01
Mikhail Anishchanka <vistrible@yandex.ru>
2018-08-15 12:53:30 +0300

    completed task1

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+
+const errorHandler = function(err, req, res, next) {
+  res.status(500).json({ statusCode: res.statusCode, error: err.message });
+  console.log(err);
+};
+
+app.get("/api/hello-world/:name?", (req, res, next) => {
+  if (!req.params.name) {
+    next(new Error("not found"));
+  }
+  res.render("index", { name: req.params.name });
+});
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () =>
+  console.log("App is listening on port 3000!")
+);
